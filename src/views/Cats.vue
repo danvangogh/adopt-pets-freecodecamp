cdb8868eb885754317b8f30d57ce150449a7f0bf
danvangogh <djameswhite@yahoo.com>
2019-05-16 07:24:25 -0700

    vuex store

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -3,20 +3,25 @@
     <h1>Cats for Adoption</h1>
     <b-table striped hover :items="cats">
       <template slot="name" slot-scope="data">
-      <router-link :to="`/pets/${data.index}`">{{ data.value }}</router-link>
+        <router-link :to="`/pets/${data.index}`">
+          {{ data.value }}
+        </router-link>
       </template>
     </b-table>
   </div>
 </template>
 
 <script>
-import cats from '@/data/cats'
+  import { mapState } from 'vuex'
 
-export default {
-  data() {
-    return {
-      cats
+  export default {
+    data() {
+      return {}
+    },
+    computed: {
+      ...mapState([
+        'cats'
+      ])
     }
   }
-}
 </script>
